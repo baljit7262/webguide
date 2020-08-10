7b53e0e23d7564113e0b14bb9c293418c11fd418
Parminder <sunitakatoch20@gmail.com>
2020-08-10 19:27:15 +0530

    added description

--- src/app/layout/master/master.module.ts
+++ src/app/layout/master/master.module.ts
@@ -1,25 +1,28 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
-// import { MasterComponent } from './master.component';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { CommonModule } from '@angular/common';
-// import { HttpClientModule } from '@angular/common/http';
-// import {MatSidenavModule} from '@angular/material/sidenav';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MasterComponent } from './master.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
 
 
-// @NgModule({
-//     imports: [
-//         CommonModule,
-//         BrowserModule,
-//         HttpClientModule,
-//         MatSidenavModule,
-//     ],
 
-//     declarations: [
-//         MasterComponent
-//     ],
-//     exports: [RouterModule],
-//     providers: []
-// })
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        HttpClientModule,
+        MatSidenavModule,
+        MatMenuModule,
+        ],
 
-// export class MasterModule { }
+    declarations: [
+        MasterComponent
+    ],
+    exports: [RouterModule],
+    providers: []
+})
+
+export class MasterModule { }
